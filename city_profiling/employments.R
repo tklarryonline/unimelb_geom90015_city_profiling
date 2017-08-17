de4ff1b62edca8ef245b1347a9d3bc1c2cfc520f
Luan Nguyen <luan@tklarryonline.me>
2017-08-17 20:13:13 +1000

    [f] Reading the GML file from WFS

---
+++ city_profiling/employments.R
@@ -0,0 +1,8 @@
+# Author: Luan Thanh Nguyen
+# Email:  luan@tklarryonline.me
+
+# Source the manage.R file to setup project
+source("./manage.R")
+
+vic.employment.url <- "http://115.146.92.236:8080/geoserver/group2/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=group2:vic_employment_by_sex_and_hours_aus_sa2_2011_ste_gccsa_info&ouputFormat=JSON"
+
---
+++ city_profiling/manage.R
@@ -0,0 +1,42 @@
+# Author: Luan Thanh Nguyen
+# Email:  luan@tklarryonline.me
+#
+# The purpose of this file is to install/init all the required packages
+# And source the correct util files
+
+constants.REQUIRE_PACKAGES <- c(
+  # Required ones for Dr. Ben's utils work
+  "maptools",
+  "rgdal",
+  "rgeos",
+  "geojsonio",
+  "RCurl",
+  "uuid",
+  "jsonlite",
+  "XML",
+
+  # Local ones
+  "doParallel",
+  "igraph"
+)
+
+utils.install.package <- function(package) {
+  if (!require(package)) {
+    install.packages(package)
+  }
+}
+
+for (package in constants.REQUIRE_PACKAGES) {
+  utils.install.package(package)
+}
+
+# Loads the file from Dr. Ben
+source(file.path(getwd(), "utils", "rgs_utils.R"))
+
+# Loads left packages
+library(doParallel)
+library(igraph)
+
+# Set number of cores
+constants.CORES = 1
+registerDoParallel(cores = constants.CORES)
